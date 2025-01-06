b11ab854d498a8f0da21c5b169ba7339e0f2ba93
seonggon do <mooh2jj@naver.com>
2025-01-07 02:55:03 +0900

    font_url 설정 수정

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,2 +1,2 @@
-export const API_SERVER_HOST = 'http://localhost:8080';
-export const FRONT_HOST = 'http://localhost:3000';
+export const API_SERVER_HOST = process.env.REACT_APP_API_URL;
+export const FRONT_HOST = process.env.REACT_APP_FRONT_URL;
