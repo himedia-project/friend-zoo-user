93340c9203bd711d8f568cfed23e068640b3a389
seonggon do <mooh2jj@naver.com>
2025-01-07 17:52:20 +0900

    kakao api getKakaoLoginLink 수정

--- src/api/kakaoApi.js
+++ src/api/kakaoApi.js
@@ -14,11 +14,19 @@ const auth_code_path = `https://kauth.kakao.com/oauth/authorize`;
 // access token 요청 url
 const access_token_url = `https://kauth.kakao.com/oauth/token`;
 
+// 환경변수에서 값을 제대로 가져오는지 디버깅을 위해 로깅
+console.log('REST API KEY:', process.env.REACT_APP_KAKAO_REST_API_KEY);
+console.log('FRONT HOST:', FRONT_HOST);
+
 // 카카오 로그인 요청 -> 카카오 로그인 페이지(리다이렉트 페이지)로 이동
 export const getKakaoLoginLink = () => {
-  const kakaoURL = `${auth_code_path}?client_id=${rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
-  console.log('getKakaoLoginLink kakaoURL: ', kakaoURL);
-  return kakaoURL;
+  const kakaoURL = new URL(auth_code_path);
+  kakaoURL.searchParams.append('client_id', rest_api_key);
+  kakaoURL.searchParams.append('redirect_uri', redirect_uri);
+  kakaoURL.searchParams.append('response_type', 'code');
+
+  console.log('getKakaoLoginLink kakaoURL: ', kakaoURL.toString());
+  return kakaoURL.toString();
 };
 
 // 카카오 로그인 후 code를 받고, 서버가 대신 kakao oauth2서버에 access_token 요청
