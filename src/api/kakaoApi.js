b19beb1ead011d89eb5f647f0ba5e1134fa68f22
seonggon do <mooh2jj@naver.com>
2025-01-07 20:46:28 +0900

    카카오 api 디버깅

--- src/api/kakaoApi.js
+++ src/api/kakaoApi.js
@@ -6,7 +6,7 @@ const rest_api_key = process.env.REACT_APP_KAKAO_REST_API_KEY;
 const client_secret = process.env.REACT_APP_KAKAO_CLIENT_SECRET;
 
 // 리다이렉트 uri => kakaoRedirectPage로 이동
-const redirect_uri = `http://43.200.239.220:3000/member/kakao`; // http://43.200.239.220:3000/member/kakao
+const redirect_uri = `${FRONT_HOST}/member/kakao`; // http://43.200.239.220:3000/member/kakao
 
 // 인증 code 요청 url
 const auth_code_path = `https://kauth.kakao.com/oauth/authorize`;
@@ -15,8 +15,9 @@ const auth_code_path = `https://kauth.kakao.com/oauth/authorize`;
 const access_token_url = `https://kauth.kakao.com/oauth/token`;
 
 // 환경변수에서 값을 제대로 가져오는지 디버깅을 위해 로깅
-console.log('REST API KEY:', process.env.REACT_APP_KAKAO_REST_API_KEY);
+console.log('REST API KEY:', rest_api_key);
 console.log('FRONT HOST:', FRONT_HOST);
+console.log('REDIRECT URI:', redirect_uri);
 
 // 카카오 로그인 요청 -> 카카오 로그인 페이지(리다이렉트 페이지)로 이동
 export const getKakaoLoginLink = () => {
@@ -31,21 +32,34 @@ export const getKakaoLoginLink = () => {
 
 // 카카오 로그인 후 code를 받고, 서버가 대신 kakao oauth2서버에 access_token 요청
 export const getAccessToken = async (authCode) => {
-  const res = await axios.get(
-    `${API_SERVER_HOST}/api/member/kakao/token?code=${authCode}`,
-  );
-  console.log('getAccessToken res: ', res);
-  return res.data;
+  try {
+    const res = await axios.get(
+      `${API_SERVER_HOST}/api/member/kakao/token?code=${authCode}`,
+    );
+    console.log('getAccessToken res: ', res);
+    return res.data;
+  } catch (error) {
+    console.error('Error fetching access token:', error);
+    throw error;
+  }
 };
 
 // 카카오 사용자 정보 서버에 요청
 export const getMemberWithAccessToken = async (accessToken) => {
-  const header = {
-    withCredentials: true,
+  const headers = {
+    headers: {
+      withCredentials: true,
+    },
   };
-  const res = await axios.get(
-    `${API_SERVER_HOST}/api/member/kakao?accessToken=${accessToken}`,
-    header,
-  );
-  return res.data;
+  try {
+    const res = await axios.get(
+      `${API_SERVER_HOST}/api/member/kakao?accessToken=${accessToken}`,
+      headers,
+    );
+    console.log('getMemberWithAccessToken res: ', res);
+    return res.data;
+  } catch (error) {
+    console.error('Error fetching member data:', error);
+    throw error;
+  }
 };
